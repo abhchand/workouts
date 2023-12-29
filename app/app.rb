7c3f60cc40e78a48c0a182da921ac52eade22574
abhchand <me@abhchand.me>
2023-12-29 15:48:51 -0500

    Define `APP_ROOT` and programmatically load models

--- app/app.rb
+++ app/app.rb
@@ -3,34 +3,23 @@ require "sinatra/activerecord"
 require "sinatra/flash"
 require "active_support"
 
-require_relative "models/challenge"
-require_relative "models/person"
-require_relative "models/person_challenge"
-require_relative "models/workout"
+require "pathname"
 
+APP_ROOT = Pathname.new(File.expand_path("..", __dir__))
 SQLITE_FILE = ENV["SQLITE_FILE_NAME"] || "app.sqlite3"
-set :database, { adapter: "sqlite3", database: SQLITE_FILE }
 
+## Application Configuration
+
+set :database, { adapter: "sqlite3", database: SQLITE_FILE }
 enable :sessions
 
 Time.zone = "UTC"
 
-get "/" do
-  @widgets = Widget.all.order(:created_at)
+# Application Resources
 
+Dir[APP_ROOT.join("app", "models", "*")].each { |model| require model }
+
+get "/" do
   erb :index
 end
 
-post "/widget" do
-  Time.zone = "UTC"
-
-  name = params[:name]
-  if name.nil? || name == ""
-    flash[:error] = "PLEASE ENTER YOUR NAME"
-    redirect to("/")
-    return
-  end
-
-  Widget.create!(name: name)
-  redirect to("/")
-end

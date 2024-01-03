b054615e5d4a20ec4cc6259363daec814bf1f58c
abhchand <me@abhchand.me>
2024-01-03 00:03:05 -0500

    Add `challenges#new`

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -4,10 +4,44 @@ get "/challenges" do
   erb :"challenges/index"
 end
 
+get "/challenges/new" do
+  erb :"challenges/new"
+end
+
 get "/challenges/:challenge_id" do
   @challenge = Challenge.find(params['challenge_id'])
   @participants = @challenge.participants.includes(:workouts).order(:name)
   @workouts = @challenge.workouts.includes(:participant).order(occurred_on: :desc)
 
   erb :"challenges/show"
+end
+
+post "/challenges" do
+  participants = params["challenge"]["participant_id"].values.map { |id| Person.find(id) }
+
+  if participants.count != 2 || participants.uniq.count != 2
+    flash[:error] = "Please select unique participants"
+    redirect back
+  end
+
+  challenge_params = params["challenge"].dup
+  challenge_params.delete("participant_id")
+  challenge_params["started_at"] = Time.parse(challenge_params["started_at"])
+  challenge_params["ended_at"] = Time.parse(challenge_params["ended_at"])
+
+  challenge = Challenge.new(challenge_params)
+
+  if !challenge.valid?
+    flash[:error] = challenge.error_messages
+    redirect back
+  end
+
+  challenge.save!
+  challenge = Challenge.last
+  participants.each do |participant|
+    PersonChallenge.create!(person: participant, challenge: challenge)
+  end
+
+  flash[:success] = "Challenge created"
+  redirect "/"
 end

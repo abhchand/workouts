bd199e7d271ed060dbb5f23c7989df7d804c9193
abhchand <me@abhchand.me>
2024-01-03 00:21:36 -0500

    Scope workouts to challenge

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,6 +8,22 @@ class Person < ActiveRecord::Base
 
   before_validation :canonicalize_name
 
+  def workout_count(challenge: nil)
+    query = workouts
+    query = query.where("person_challenges.challenge_id = ?", challenge.id) if challenge
+
+    query.count
+  end
+
+  def cost_of(challenge)
+    other_participant = challenge.participants.where.not(id: id).first
+
+    my_workout_count = workout_count(challenge: challenge)
+    other_workout_count = other_participant.workout_count(challenge: challenge)
+
+    (my_workout_count - other_workout_count) * challenge.cost_per_workout
+  end
+
   private
 
   def canonicalize_name
